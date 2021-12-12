1bba89ea43df124fe5635c9837aac777ff09e751
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-12-12 15:12:30 +0900

    feat: 길찾기 눌렀을 때 파라미터 받는 기능 완성

--- src/main/java/com/example/avoiddangerousroad/controller/HomeController.java
+++ src/main/java/com/example/avoiddangerousroad/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.avoiddangerousroad.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -10,4 +13,19 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @RequestMapping(value = "/findRoad")
+    public String findRoad(HttpServletRequest request, Model model) {
+        String startLatitude = request.getParameter("startLatitude");
+        String startLongitude = request.getParameter("startLongitude");
+        String endLatitude = request.getParameter("endLatitude");
+        String endLongitude = request.getParameter("endLongitude");
+        String[] preferences = request.getParameterValues("preference[]");
+        for (String preference : preferences) {
+            System.out.println("preference = " + preference);
+        }
+
+        return "findRoad";
+    }
+
 }
