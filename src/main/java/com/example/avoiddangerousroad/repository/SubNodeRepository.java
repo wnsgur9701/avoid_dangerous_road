60797c5b685e9b4cb1a1c2fc5846c48287b474be
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-12-12 22:17:03 +0900

    feat: subNodeRepository 완성

---
+++ src/main/java/com/example/avoiddangerousroad/repository/SubNodeRepository.java
@@ -0,0 +1,7 @@
+package com.example.avoiddangerousroad.repository;
+
+import com.example.avoiddangerousroad.entity.SubNode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubNodeRepository extends JpaRepository<SubNode, Long> {
+}
