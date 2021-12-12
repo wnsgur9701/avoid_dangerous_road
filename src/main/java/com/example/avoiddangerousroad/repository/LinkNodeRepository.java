bc6100d35b0475086d6aa600b62a56652f3ce432
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-12-12 22:16:43 +0900

    feat: linkNodeRepository 생성

---
+++ src/main/java/com/example/avoiddangerousroad/repository/LinkNodeRepository.java
@@ -0,0 +1,7 @@
+package com.example.avoiddangerousroad.repository;
+
+import com.example.avoiddangerousroad.entity.LinkNode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkNodeRepository extends JpaRepository<LinkNode, Long> {
+}
