31682386c4b34c9d62f68b78006bbfcdd35d255b
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-12-12 21:50:21 +0900

    feat: NodeRepository 생성

---
+++ src/main/java/com/example/avoiddangerousroad/repository/NodeRepository.java
@@ -0,0 +1,7 @@
+package com.example.avoiddangerousroad.repository;
+
+import com.example.avoiddangerousroad.entity.Node;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NodeRepository extends JpaRepository<Node,Long> {
+}
