13769274831a323ba46c3b50b038f12417fba5fa
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-12-17 15:30:18 +0900

    chore: controller inport 취소

--- src/main/java/com/example/avoiddangerousroad/controller/EntityRegisterController.java
+++ src/main/java/com/example/avoiddangerousroad/controller/EntityRegisterController.java
@@ -4,7 +4,6 @@ import com.example.avoiddangerousroad.service.LinkService;
 import com.example.avoiddangerousroad.service.NodeService;
 import com.example.avoiddangerousroad.service.SubNodeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
